3f89f8c7d6cd22abb7b93782ce5831f9b17adf8f
Suchet Iyer <iyer.suchet@gmail.com>
2020-05-07 02:29:24 +0530

    moved to using query parameters

--- calculator.js
+++ calculator.js
@@ -2,35 +2,50 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/api', (req, res) => res.send('Calculator, express.js'))
+app.get('/', (req, res) => res.send('Calculator health check, express.js'))
 
-app.get('/math/:operation/:num1/:num2' , function (req, res) {
-    var operation = req.params.operation;
-	var n1 = parseFloat(req.params.num1);
-    var n2 = parseFloat(req.params.num2);
+// app.get('/math/:operation/:num1/:num2' , function (req, res) {
+app.get('/math', function(req,res) {
+    var operation = req.query.op;
+    var nums = JSON.parse("[" + req.query.nums + "]");
+    console.log(nums);
+    // var operation = req.params.operation;
+	// var n1 = parseFloat(req.params.num1);
+    // var n2 = parseArray(req.params.num2);
+    var i = 0;
     var result = 0;
     var operationName = 'null'
     switch(operation){
         case 'add':
-            result = n1 + n2;
+            for (i = 0; i < nums.length; i++) {
+                result += nums[i];
+            }
             operationName = 'Addition';
             break;
         case 'sub':
-            result = n1 - n2;
+            for (i = 0; i < nums.length; i++) {
+                result -= nums[i];
+            }
             operationName = 'Subtration';
             break;
         case 'mul': 
-            result = n1 * n2;
+            result = nums[0];
+            for (i = 1; i < nums.length; i++) {
+                result *= nums[i];
+            }
             operationName = 'Multiplication';
             break;
         case 'div': 
-            result = n1 / n2;
+            result = nums[0];
+            for (i = 1; i < nums.length; i++) {
+                result /= nums[i];
+            }
             operationName = 'Division';
             break;
         default:
             result = 'null';
     }
-    res.send(`The result of ${operationName} of ${n1} and ${n2} is equal to ${result}`);
+    res.send(`The result of ${operationName} of these numbers: [${nums}] is equal to ${result}`);
 })
 
 app.listen(port, () => console.log(`Calculator app listening at http://localhost:${port}`))
